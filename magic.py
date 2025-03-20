e5d6555cffb1ce87045cc25b6c42e535eb1e3798
Ram <nagaram.annavarapu@gmail.com>
2025-03-20 12:04:59 -0400

    Data Report generated

--- magic.py
+++ magic.py
@@ -1,12 +1,18 @@
 import pandas as pd
-
+import pandas_profiling 
+from pandas_profiling import ProfileReport
 
 cols=["fLength","fWidth","fSize","fConc","fConc1","fAsym","fM3Long","fM3Trans","fAlpha","fDist","class"]
 df=pd.read_csv("magic04.data", names=cols)
 print(df["class"].value_counts())
+
 
 #print(df.tail())
 print(df["class"].unique())
 df["class"]=df["class"].replace({'g':0, 'h':1})
 print(df.head())
 print(df["class"].value_counts())
+
+rpt=ProfileReport(df)
+rpt.to_file("Data.html")
+

54056f2a49a293b812f6c9eb5f9b21d309a94914
Ram <nagaram.annavarapu@gmail.com>
2025-03-20 12:00:00 -0400

    Binary Encoding done. Target variable converted as required

--- magic.py
+++ magic.py
@@ -1,7 +1,12 @@
 import pandas as pd
 
 
-cols=["fLength","fWidth","fSize","fConc","fConc1","fAsym","fM3Long","fM3Trans","fAlpha","fDist"]
+cols=["fLength","fWidth","fSize","fConc","fConc1","fAsym","fM3Long","fM3Trans","fAlpha","fDist","class"]
 df=pd.read_csv("magic04.data", names=cols)
+print(df["class"].value_counts())
 
+#print(df.tail())
+print(df["class"].unique())
+df["class"]=df["class"].replace({'g':0, 'h':1})
 print(df.head())
+print(df["class"].value_counts())

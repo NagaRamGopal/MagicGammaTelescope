7705781e2c1017e971a3c884138b07600868fc32
Ram <nagaram.annavarapu@gmail.com>
2025-03-24 12:54:53 -0400

    Scaling data done

--- magic.py
+++ magic.py
@@ -2,6 +2,10 @@ import pandas as pd
 import pandas_profiling 
 from pandas_profiling import ProfileReport
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+
 cols=["fLength","fWidth","fSize","fConc","fConc1","fAsym","fM3Long","fM3Trans","fAlpha","fDist","class"]
 df=pd.read_csv("magic04.data", names=cols)
 print(df["class"].value_counts())
@@ -17,14 +21,21 @@ print(df["class"].value_counts())
 #rpt=ProfileReport(df)
 #rpt.to_file("Data.html")
 
-
+'''
 for label in cols[:-1]:
-    plt.hist(df[df["class"]==0][label], color='blue', label='gamma',  alpha=0.7, density=True)
-    plt.hist(df[df["class"]==1][label], color='red', label='hadron', alpha=0.7, density=True)
+    plt.hist(df[df["class"]==0] [label], color="blue", label="gamma", density=True, alpha=0.7)
+    plt.hist(df[df["class"]==1] [label], color="red", label="hedron", density=True, alpha=0.7)
     plt.title(label)
     plt.ylabel("Probability")
     plt.xlabel(label)
     plt.legend()
     plt.show()
+'''
 
+train,valid,test=np.split(df.sample(frac=1), [int(0.6*len(df)), int(0.8*len(df))])
 
+x=df[cols[:-1]].values
+y=df[cols[-1]].values
+scaler=StandardScaler()
+X=scaler.fit_transform(x)
+print(X)
